441125225bbc53aeb6664bd6de1f861f99907aae
runonce <camilorivera86@gmail.com>
2018-06-21 10:56:32 -0300

    Added indeces and constraints

--- database/migrations/2018_06_19_234650_create_person_role_table.php
+++ database/migrations/2018_06_19_234650_create_person_role_table.php
@@ -15,9 +15,17 @@ class CreatePersonRoleTable extends Migration
     {
         Schema::create('person_role', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('person_id');
-            $table->integer('role_id');
+            $table->integer('person_id')->index();
+            $table->integer('role_id')->index();
             $table->timestamps();
+
+            $table->foreign('person_id')
+                  ->references('id')->on('people')
+                  ->onDelete('cascade');
+
+            $table->foreign('role_id')
+                  ->references('id')->on('roles')
+                  ->onDelete('cascade');
         });
     }
 
