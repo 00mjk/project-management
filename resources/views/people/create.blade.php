b8c34ae5b97b0de0e8bda809003d3404ea58aada
runonce <camilorivera86@gmail.com>
2018-07-11 20:47:16 -0300

    Added default empty value to Role select input

--- resources/views/people/create.blade.php
+++ resources/views/people/create.blade.php
@@ -20,7 +20,7 @@
 
         <div class="form-group">
             <label for="role_id">Role</label>
-            {{ Form::select('role_id', $roles, null, ['class' => 'form-control']) }}
+            {{ Form::select('role_id', $roles, null, ['class' => 'form-control', 'placeholder' => 'Select role...']) }}
         </div>
 
         <p class="text-right"><button type="submit" id="create" class="btn btn-primary">Create new person</button></p>
